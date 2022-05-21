62ecb6437a1ba004fcee371a110966dd4770535e
Ridwan-Suhel <ridwansuhel96@gmail.com>
2022-05-21 22:37:27 +0600

    Init commit for toolsplaza server

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+const app = express();
+
+require("dotenv").config();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World! From ToolsPlaza.");
+});
+
+app.listen(port, () => {
+  console.log(`ToolsPlaza app listening on port`, port);
+});
