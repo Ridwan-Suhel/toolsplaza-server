481b698cc34b8a7a7b98e17c781aa0bed4d03134
Ridwan-Suhel <ridwansuhel96@gmail.com>
2022-05-22 18:46:10 +0600

    mongodb connected and all tools api added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // middleware
 app.use(cors());
@@ -12,6 +13,31 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello World! From ToolsPlaza.");
 });
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vxlwg.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const toolsCollection = client.db("toolsplazadb").collection("tools");
+
+    // geting all tools
+    app.get("/tools", async (req, res) => {
+      const query = {};
+      const cursor = toolsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`ToolsPlaza app listening on port`, port);
