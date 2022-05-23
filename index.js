0061f137d02f26f9bdb76820e1f029d900189287
Ridwan-Suhel <ridwansuhel96@gmail.com>
2022-05-24 02:48:27 +0600

    geting orders collection for specific user ordered id for payment

--- index.js
+++ index.js
@@ -46,17 +46,26 @@ async function run() {
     //posting what user orders on mongodb
     app.post("/orders", async (req, res) => {
       const orders = req.body;
-      console.log(orders);
+      // console.log(orders);
       const result = await ordersCollection.insertOne(orders);
       return res.send(result);
     });
 
-    //geting specific data from orders using email query
+    //geting specific data from orders using email params
     app.get("/orders/:email", async (req, res) => {
       const email = req.params.email;
       const query = { email: email };
       const orders = (await ordersCollection.find(query).toArray()).reverse();
       res.send(orders);
+    });
+
+    // geting orders collection for specific user ordered id for payment
+    app.get("/orders/order/:id", async (req, res) => {
+      const id = req?.params?.id;
+      // console.log("Hello Id- ", id);
+      const query = { _id: ObjectId(id) };
+      const order = await ordersCollection.findOne(query);
+      res.send(order);
     });
   } finally {
     // await client.close();
